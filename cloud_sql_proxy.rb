021bc196a18f851295fb5a57f42e36eb8e7bc5a4
Richard Lee <14349+dlackty@users.noreply.github.com>
2021-03-06 21:38:13 +0800

    Bump version to 1.20.2

--- cloud_sql_proxy.rb
+++ cloud_sql_proxy.rb
@@ -1,24 +1,14 @@
 class CloudSqlProxy < Formula
   desc "The Cloud SQL Proxy for GoogleCloudPlatform"
   homepage "https://github.com/GoogleCloudPlatform/cloudsql-proxy"
-  version "v1.17"
-  url "https://github.com/GoogleCloudPlatform/cloudsql-proxy/archive/#{version}.tar.gz"
-  sha256 "241f5b7a2118bfd1a305b249e95f67b4bbedb3ee62783b492d3d9e445d7c12e1"
+  version "1.20.2"
+  url "https://github.com/GoogleCloudPlatform/cloudsql-proxy/archive/v#{version}.tar.gz"
+  sha256 "a2dca081982b155ba5bf2604b438321b9d3bc2e9502e944e43ee988359cb7de6"
   head "https://github.com/GoogleCloudPlatform/cloudsql-proxy.git"
   
   depends_on "go" => :build
-  depends_on "git" => :build
 
   def install
-    ENV["GOPATH"] = buildpath
-    (buildpath/"src/github.com/GoogleCloudPlatform/cloud_sql_proxy").install buildpath.children
-    
-    cd "src/github.com/GoogleCloudPlatform/cloud_sql_proxy/cmd/cloud_sql_proxy" do
-      system "go get"
-      system "go build"
-      system "mkdir bin"
-      system "mv cloud_sql_proxy bin/cloud_sql_proxy"
-      bin.install "bin/cloud_sql_proxy"
-    end
+    system "go", "build", "-o", bin/"cloud_sql_proxy", "./cmd/cloud_sql_proxy"
   end
 end
