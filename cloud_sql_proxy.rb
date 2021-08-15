90be0d67168edae0fb68e405262d17890c832e2e
gen16k <38280986+gen16k@users.noreply.github.com>
2021-08-15 18:18:52 +0900

    fix sha256

--- cloud_sql_proxy.rb
+++ cloud_sql_proxy.rb
@@ -3,7 +3,7 @@ class CloudSqlProxy < Formula
   homepage "https://github.com/GoogleCloudPlatform/cloudsql-proxy"
   version "1.24.0"
   url "https://github.com/GoogleCloudPlatform/cloudsql-proxy/archive/v#{version}.tar.gz"
-  sha256 "fd0dd769d48577e74522a8206032ef5ae80c34d5273ee7ea1d707a4e3dffec0c"
+  sha256 "7006685f0bb23c1dd519da139ea341c34bc294e28924d15302112f93823cf5bb"
   head "https://github.com/GoogleCloudPlatform/cloudsql-proxy.git"
   
   depends_on "go" => :build
