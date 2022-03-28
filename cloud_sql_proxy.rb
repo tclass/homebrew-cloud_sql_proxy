64d90ebe77dd231c77b3bfcd39acb73df706e8f1
Tobi <tobias.klasse@sesamecare.com>
2022-03-28 15:26:09 +0200

    Update cloud_sql_proxy.rb

--- cloud_sql_proxy.rb
+++ cloud_sql_proxy.rb
@@ -3,7 +3,7 @@ class CloudSqlProxy < Formula
   homepage "https://github.com/GoogleCloudPlatform/cloudsql-proxy"
   version "1.29.0"
   url "https://github.com/GoogleCloudPlatform/cloudsql-proxy/archive/v#{version}.tar.gz"
-  sha256 "77dd4486a6e8bf2ea237f562477828c2e3416002da2a7f648bf793fd900ba8f8"
+  sha256 "a44fe7cdc9b98eb9f4fda92207490de6ab52de8a00e6a4704d5801187b6aa3bb"
   head "https://github.com/GoogleCloudPlatform/cloudsql-proxy.git"
   
   depends_on "go" => :build
