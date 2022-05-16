6ca40513b802343470e1529a79d730ab6e142bee
Phil Brocker <phil.brocker@outreach.io>
2022-05-16 11:55:25 -0400

    bump minor version and add test

--- cloud_sql_proxy.rb
+++ cloud_sql_proxy.rb
@@ -1,14 +1,18 @@
 class CloudSqlProxy < Formula
   desc "The Cloud SQL Proxy for GoogleCloudPlatform"
   homepage "https://github.com/GoogleCloudPlatform/cloudsql-proxy"
-  version "1.30.0"
+  version "1.30.1"
   url "https://github.com/GoogleCloudPlatform/cloudsql-proxy/archive/v#{version}.tar.gz"
-  sha256 "071b3b13fcc0bc1a9c9cd33c69930e8d6fcd89d089c653ecbfc39ca02008acb7"
+  sha256 "1504266f1e6917734c7a5e11e171e9824114de88c5a1dfcc654d6dbeff7bbad7"
   head "https://github.com/GoogleCloudPlatform/cloudsql-proxy.git"
   
   depends_on "go" => :build
 
   def install
     system "go", "build", "-o", bin/"cloud_sql_proxy", "./cmd/cloud_sql_proxy"
+  end
+
+  test do
+    system "cloud_sql_proxy -version"
   end
 end
