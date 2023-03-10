74775a04981f985c418871398220a8bbd1414697
Filip Wojciechowski <fwojciec@gmail.com>
2023-03-10 02:56:20 -0800

    fix build path (#14)

--- cloud_sql_proxy.rb
+++ cloud_sql_proxy.rb
@@ -5,11 +5,11 @@ class CloudSqlProxy < Formula
   url "https://github.com/GoogleCloudPlatform/cloud-sql-proxy/archive/refs/tags/v#{version}.tar.gz"
   sha256 "375e577e993e72f8c35d43e2b0672862f3bb10c1ca49fcc7c1f98b9e42fb7261"
   head "https://github.com/GoogleCloudPlatform/cloud-sql-proxy.git"
-  
+
   depends_on "go" => :build
 
   def install
-    system "go", "build", "-o", bin/"cloud_sql_proxy", "./cmd/cloud_sql_proxy"
+    system "go", "build", "-o", bin/"cloud_sql_proxy", "."
   end
 
   service do
