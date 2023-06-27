8dee7adf808e0cee08545a6dfff64c1a9aaf3c13
Phil Brocker <phil.brocker@outreach.io>
2023-06-27 13:48:22 -0400

    bump to v2.4.0

--- cloud_sql_proxy.rb
+++ cloud_sql_proxy.rb
@@ -1,9 +1,9 @@
 class CloudSqlProxy < Formula
   desc "The Cloud SQL Proxy for GoogleCloudPlatform"
   homepage "https://github.com/GoogleCloudPlatform/cloudsql-proxy"
-  version "2.2.0"
+  version "2.4.0"
   url "https://github.com/GoogleCloudPlatform/cloud-sql-proxy/archive/refs/tags/v#{version}.tar.gz"
-  sha256 "83c3e801d9c153af47c74686e9d900e04adcbdb685328aad194bf91b55adc17c"
+  sha256 "dfa23097db01f76f6bda1b433d3d2caa6929bcbc22c909577f4fcd20395c0310"
   head "https://github.com/GoogleCloudPlatform/cloud-sql-proxy.git"
 
   depends_on "go" => :build
