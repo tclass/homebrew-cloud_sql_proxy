734c5c86568a5578c4cea70794cbf9d09975a9e4
Phil Brocker <5331286+pbr0ck3r@users.noreply.github.com>
2023-07-29 04:07:26 -0400

    feat: bump version to 2.6.0 (#18)

--- cloud_sql_proxy.rb
+++ cloud_sql_proxy.rb
@@ -1,9 +1,9 @@
 class CloudSqlProxy < Formula
   desc "The Cloud SQL Proxy for GoogleCloudPlatform"
   homepage "https://github.com/GoogleCloudPlatform/cloudsql-proxy"
-  version "2.4.0"
+  version "2.6.0"
   url "https://github.com/GoogleCloudPlatform/cloud-sql-proxy/archive/refs/tags/v#{version}.tar.gz"
-  sha256 "dfa23097db01f76f6bda1b433d3d2caa6929bcbc22c909577f4fcd20395c0310"
+  sha256 "53626d199051074f4be93f8e9e4d7e390d627328c8e492c44577fe675629f245"
   head "https://github.com/GoogleCloudPlatform/cloud-sql-proxy.git"
 
   depends_on "go" => :build
